d906598ccb60512b743593ac0c3a39e9319e0555
James Burns <jrburns96@aol.com>
2021-12-13 19:49:23 -0500

    commit tree 54b3c43f8d0a05c260ac8e07bfc0c66f817d1431

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,51 @@
+# require 
 def admin_login(username, password)
-  # your code here
+  if username.downcase == 'admin' && password != 'sudo'
+    "Access granted"
+  else
+    'Access denied'
+  end
 end
-
 def hows_the_weather(temperature)
-  # your code here
-end
+  case temperature
+  when 1..40 then "It's brisk out there!"
+  when 41..65 
+    "It's a little chilly out there!"
+  when 85..100
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
+  end
 
 def fizzbuzz(num)
-  # your code here
+  if num%3 == 0 and num%5 ==0
+    "FizzBuzz"
+  elsif num%3 ==0
+    "Fizz"
+  elsif num%5 ==0
+    "Buzz"
+  else
+    num
+end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+# num1 - num2
+# end
+    if operation == '+'
+    num1 + num2
+    elsif operation == '-'
+      num1 - num2
+    elsif operation == '/'
+        num1 / num2
+    elsif operation == '*'
+          num1 * num2
+        else
+    p 'Invalid operation!'
+    nil
+end
 end
 
+
+p calculator("-", 1, 2)
